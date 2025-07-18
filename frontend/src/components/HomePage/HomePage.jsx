525cb9e6cbf528d91c81cf68b966742a8b313048
Bihari-Fernando <biharifernando2000@gmail.com>
2025-07-18 13:46:24 +0530

    rename 'items' to 'products' for better clarity

--- frontend/src/components/HomePage/HomePage.jsx
+++ frontend/src/components/HomePage/HomePage.jsx
@@ -4,33 +4,40 @@ import axios from "axios";
 import Hero from "../Common/Hero/Hero";
 import ProductCardGrid from "../Common/Products/ProductCardGrid";
 import ItemCardGrid from "../Common/Items/ItemCardGrid";
+import Navbar from "../Common/Navbar/Navbar";
+import CartDrawer from "../Common/Cart/CartDrawer";
+import Footer from "../Common/Footer/Footer";
 
 const HomePage = () => {
+  const [categories, setCategories] = useState([]);
   const [products, setProducts] = useState([]);
-  const [items, setItems] = useState([]);
+  
 
   useEffect(() => {
     axios
       .get("http://localhost:8000/api/categories")
-      .then((res) => setProducts(res.data))
-      .catch((err) => console.error("Failed to fetch products:", err));
+      .then((res) => setCategories(res.data))
+      .catch((err) => console.error("Failed to fetch categories:", err));
   }, []);
 
   useEffect(() => {
     axios
       .get("http://localhost:8000/api/products")
-      .then((res) => setItems(res.data))
+      .then((res) => setProducts(res.data))
       .catch((err) => console.error("Failed to fetch items:", err));
   }, []);
 
   return (
     <div>
+      <Navbar/>
+      <CartDrawer/>
       <Hero />
-      <ProductCardGrid products={products} />
+      <ProductCardGrid categories={categories} />
       <div>
         <p className="text-3xl font-semibold text-center mb-8">Top Items</p>
       </div> 
-      <ItemCardGrid items={items} />
+      <ItemCardGrid products={products} />
+      <Footer/>
     </div>
   );
 };
