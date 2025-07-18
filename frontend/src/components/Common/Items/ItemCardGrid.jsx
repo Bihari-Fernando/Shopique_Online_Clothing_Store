2542d16bc290f607b17a4c2f634b0d9c3d82d780
Bihari-Fernando <biharifernando2000@gmail.com>
2025-07-18 13:53:53 +0530

    rename 'items' to 'products' for better clarity

--- frontend/src/components/Common/Items/ItemCardGrid.jsx
+++ frontend/src/components/Common/Items/ItemCardGrid.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import ItemCard from './ItemCard'
 
-const ItemCardGrid = ({ items }) => {
+const ItemCardGrid = ({ products }) => {
   return (
     <div className="m-20 dark:bg-gray-900 dark:text-white"> 
       <div className="flex flex-wrap justify-center gap-8">
-         {items.length > 0 ? (
-          items.map((item, index) => <ItemCard key={index} item={item} />)
+         {products.length > 0 ? (
+          products.map((product, index) => <ItemCard key={index} product={product} />)
         ) : (
           <p className="text-center">No items match your filters.</p>
         )}
