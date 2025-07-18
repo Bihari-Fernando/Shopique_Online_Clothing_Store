c464ed7208864a7e75bad473bcb8489cfba923cb
Bihari-Fernando <biharifernando2000@gmail.com>
2025-07-18 13:54:28 +0530

    rename 'item' to 'product' for better clarity

--- frontend/src/components/Common/Items/ItemCard.jsx
+++ frontend/src/components/Common/Items/ItemCard.jsx
@@ -3,22 +3,23 @@ import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 import { Link } from "react-router-dom";
 
-const ItemCard = ({ item }) => {
+
+const ItemCard = ({ product }) => {
   return (
     <div className="col-12 col-sm-6 col-md-4 col-lg-3 d-flex justify-content-center mb-4">
       <Card style={{ width: '16rem' }} className="border-1 m-2 shadow">
         <Card.Img 
           variant="top" 
-          src={item.image} 
+          src={`http://localhost:8000${product.image}`} 
           style={{ height: '200px', objectFit: 'cover' }} 
         />
         <Card.Body>
-          <Card.Title>{item.name}</Card.Title>
+          <Card.Title>{product.name}</Card.Title>
           <Card.Text>
-            Price: ${item.price} <br />
-            Size: {item.size}
+            Price: ${product.price} <br />
+            Size: {product.size}
           </Card.Text>
-          <Button  as={Link} to={`/item/${item.id}`} state={{ item }} variant="primary">View</Button>
+          <Button  as={Link} to={`/product/${product.id}`} state={{ product }} variant="primary">View</Button>
         </Card.Body>
       </Card>
     </div>
