098dac068afd6a52aa3dbac2a1d7f4da6cfede01
Bihari-Fernando <biharifernando2000@gmail.com>
2025-07-18 13:55:57 +0530

    rename 'item' to 'product' for better clarity

--- frontend/src/components/Common/Cart/CartContext.jsx
+++ frontend/src/components/Common/Cart/CartContext.jsx
@@ -1,43 +1,42 @@
-// src/components/Cart/CartContext.jsx
 import React, { createContext, useState } from "react";
 
 export const CartContext = createContext();
 
 export const CartProvider = ({ children }) => {
   const [cartItems, setCartItems] = useState([]);
-  const [isCartOpen, setIsCartOpen] = useState(false); // ✅
+  const [isCartOpen, setIsCartOpen] = useState(false); 
 
   const toggleCart = () => {
-    setIsCartOpen((prev) => !prev); // ✅
+    setIsCartOpen((prev) => !prev); 
   };
 
-  const addToCart = (item) => {
+  const addToCart = (product) => {
     setCartItems((prev) => {
       const existingItem = prev.find(
         (i) =>
-          i.id === item.id &&
-          i.name === item.name &&
-          i.size === item.size &&
-          i.color === item.color
+          i.id === product.id &&
+          i.name === product.name &&
+          i.size === product.size &&
+          i.color === product.color
       );
 
       if (existingItem) {
         return prev.map((i) =>
-          i.id === item.id &&
-          i.name === item.name &&
-          i.size === item.size &&
-          i.color === item.color
+          i.id === product.id &&
+          i.name === product.name &&
+          i.size === product.size &&
+          i.color === product.color
             ? { ...i, quantity: i.quantity + 1 }
             : i
         );
       } else {
-        return [...prev, { ...item, quantity: 1 }];
+        return [...prev, { ...product, quantity: 1 }];
       }
     });
   };
 
   const removeFromCart = (id) => {
-    setCartItems((prev) => prev.filter((item) => item.id !== id));
+    setCartItems((prev) => prev.filter((product) => product.id !== id));
   };
 
   const clearCart = () => {
