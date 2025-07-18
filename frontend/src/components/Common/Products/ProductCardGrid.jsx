ca34584a06164b1dccb975f7f7f4bdff8088339f
Bihari-Fernando <biharifernando2000@gmail.com>
2025-07-18 13:48:52 +0530

    rename 'products' to 'categories' for better clarity

--- frontend/src/components/Common/Products/ProductCardGrid.jsx
+++ frontend/src/components/Common/Products/ProductCardGrid.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ProductCard from './ProductCard';
 import Darkmode from '../Navbar/Darkmode';
 
-const ProductCardGrid = ({ products }) => {
+const ProductCardGrid = ({ categories }) => {
   return (
     <div className="m-20 dark:bg-gray-900 dark:text-white">
       <div>
@@ -10,8 +10,8 @@ const ProductCardGrid = ({ products }) => {
 
       </div>  
       <div className="flex flex-wrap justify-center gap-4">
-        {products.length > 0 ? (
-          products.map((product, index) => <ProductCard key={index} product={product} />)
+        {categories.length > 0 ? (
+          categories.map((category, index) => <ProductCard key={index} category={category} />)
         ) : (
           <p className="text-center">No items match your filters.</p>
         )}
