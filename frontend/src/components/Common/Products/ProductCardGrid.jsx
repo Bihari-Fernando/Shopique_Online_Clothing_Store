bf7ef7cd607fb6cf73a6fbd3e65464c666930843
Bihari-Fernando <biharifernando2000@gmail.com>
2025-08-02 13:25:53 +0530

    change product card styles

--- frontend/src/components/Common/Products/ProductCardGrid.jsx
+++ frontend/src/components/Common/Products/ProductCardGrid.jsx
@@ -1,17 +1,17 @@
 import React from 'react';
 import ProductCard from './ProductCard';
-import Darkmode from '../Navbar/Darkmode';
 
 const ProductCardGrid = ({ categories }) => {
   return (
-    <div className="m-20 dark:bg-gray-900 dark:text-white">
+    <div className="py-10 px-4 dark:bg-gray-900 dark:text-white">
       <div>
-        <p className="text-3xl font-semibold text-center mb-8">Products</p>
-
+        <p className="text-3xl font-semibold text-center mb-8">Categories</p>
       </div>  
       <div className="flex flex-wrap justify-center gap-4">
         {categories.length > 0 ? (
-          categories.map((category, index) => <ProductCard key={index} category={category} />)
+          categories.map((category) => (
+            <ProductCard key={category.id || category.name} category={category} />
+          ))
         ) : (
           <p className="text-center">No items match your filters.</p>
         )}
